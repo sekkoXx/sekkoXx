fb89a72965f80dbce423813a42fda7726d376de3
squinenao <squinenao2024@alu.uct.cl>
2025-04-19 15:07:04 -0400

    nada

--- schemas/vuelo_schema.py
+++ schemas/vuelo_schema.py
@@ -2,9 +2,11 @@ from pydantic import BaseModel, Field
 from typing import Optional
 
 class VueloBase(BaseModel):
+    id: int = Field(..., example=1)
     codigo: str = Field(..., example="LA1234")
     destino: str = Field(..., example="Santiago")
     emergencia: Optional[int] = Field(0, ge=0, le=1, example=1)
+    
 
 class VueloCrear(VueloBase):
     pass  # Igual al VueloBase, pero puede ser extendido en el futuro
